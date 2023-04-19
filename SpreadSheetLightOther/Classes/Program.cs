2bcc3a4375199b04cb28a05849f4e0de9cc57bfd
Karen Payne <paynekaren@comcast.net>
2023-04-19 15:22:40 -0700

    minor updates

---
+++ SpreadSheetLightOther/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using ConsoleHelperLibrary.Classes;
+using Spectre.Console;
+
+// ReSharper disable once CheckNamespace
+namespace SpreadSheetLightOther;
+internal partial class Program
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        AnsiConsole.MarkupLine("");
+        Console.Title = "Code sample";
+        WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center);
+    }
+}
--- SpreadSheetLightOther/Program.cs
+++ SpreadSheetLightOther/Program.cs
@@ -2,7 +2,7 @@
 
 namespace SpreadSheetLightOther;
 
-internal class Program
+internal partial class Program
 {
     static void Main(string[] args)
     {
