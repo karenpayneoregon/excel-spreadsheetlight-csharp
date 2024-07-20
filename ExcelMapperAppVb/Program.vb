2ae1fc4f69765e699a11ae86bb41a27cbaaf70be
Karen Payne <paynekaren@comcast.net>
2024-07-20 01:48:19 -0700

    updates

--- ExcelMapperAppVb/Program.vb
+++ ExcelMapperAppVb/Program.vb
@@ -1,14 +1,20 @@
+Imports ConsoleHelperLibrary.Classes
 Imports Ganss.Excel
+Imports Spectre.Console
 
 Module Program
     Sub Main()
+        Console.Title = "Code sample"
+        WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center)
 
         Const excelFile = "Products.xlsx"
 
         Dim excel As New ExcelMapper()
-        Dim products As List(Of Products) = excel.Fetch(Of Products)(excelFile, "Products").ToList()
+        Dim products As List(Of Products) = excel.
+                Fetch(Of Products)(excelFile, "Products").
+                ToList()
 
-
-        Console.WriteLine("Done")
+        AnsiConsole.MarkupLine("[cyan]Done[/]")
+        Console.ReadLine()
     End Sub
 End Module
